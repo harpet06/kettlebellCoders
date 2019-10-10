bb87af018ab6dba6a2769f016b27d25625469ff1
Thomas Harper <thomasharper@CIC00966.local>
2019-10-10 10:28:18 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import logo from "./kettlebell.svg";
 
 
 function App() {
   return (
     <div className="App">
-      <Header headerTitle="kettlebellCoders" />
+      <Header headerTitle="kettlebellCoders" logo={logo} expand="lg"/>
       <Main />
+      <Header fixed="bottom" />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,51 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logo from "./kettlebell.svg";
 
 function Header(props) {
   return (
     <header>
-      <Navbar bg="secondary" variant="dark" expand="lg">
+      <Navbar
+        bg="secondary"
+        variant="dark"
+        expand={props.expand}
+        fixed={props.fixed}
+      >
         <Navbar.Brand href="/">
-          <img
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt=""
-          />
+          {" "}
+          {props.logo ? (
+            <img
+              src={props.logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+          ) : null}
           {props.headerTitle}
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/workout">Workout</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-        </Nav>
+          {props.logo ? (
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/workout">Workout</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          ) : (
+            <Nav
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/workout">Workout</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Navbar>
     </header>
